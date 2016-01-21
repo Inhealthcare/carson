82b33edb0f865e4b04b484d3112b1df56d0e7e8c
Robin Tegg <robin@tegg.me.uk>
2016-01-21 22:39:27 +0000

    POC for connecting to SVN

--- carson-webapp/src/main/java/com/carson/webapp/projects/ProjectTest.java
+++ carson-webapp/src/main/java/com/carson/webapp/projects/ProjectTest.java
@@ -1,9 +1,19 @@
 package com.carson.webapp.projects;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+
+import org.jenkins.client.JenkinsClient;
+import org.tmatesoft.svn.core.SVNDirEntry;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNNodeKind;
+import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
+import org.tmatesoft.svn.core.io.SVNRepository;
+import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
+import org.tmatesoft.svn.core.wc.SVNWCUtil;
 
 public class ProjectTest {
 
@@ -35,16 +45,57 @@ public class ProjectTest {
 
 	private void testBuild() throws Exception {
 
-		URL url = new URL(project.getBuildServer().getJenkinsBuildServer().getUrl());
-		url.openConnection();
+		JenkinsClient client = new JenkinsClient(project.getBuildServer().getJenkinsBuildServer().getUrl());
+		client.getState();
 
 	}
 
 	private void testScm() throws Exception {
 
-		URL url = new URL(project.getSourceControl().getSvnRepository().getUrl());
-		url.openConnection();
+		SvnRepository repoConnection = project.getSourceControl().getSvnRepository();
 
+		SVNRepository repository = null;
+		try {
+			String url = repoConnection.getUrl();
+			repository = SVNRepositoryFactory.create(SVNURL.parseURIEncoded(url));
+			ISVNAuthenticationManager authenticationManager = SVNWCUtil
+					.createDefaultAuthenticationManager(repoConnection.getUsername(), repoConnection.getPassword());
+			repository.setAuthenticationManager(authenticationManager);
+
+			System.out.println("Repository Root: " + repository.getRepositoryRoot(true));
+			System.out.println("Repository UUID: " + repository.getRepositoryUUID(true));
+
+			SVNNodeKind nodeKind = repository.checkPath("", -1);
+			if (nodeKind == SVNNodeKind.NONE) {
+				System.err.println("There is no entry at '" + url + "'.");
+				System.exit(1);
+			} else if (nodeKind == SVNNodeKind.FILE) {
+				System.err.println("The entry at '" + url + "' is a file while a directory was expected.");
+				System.exit(1);
+			}
+
+			listEntries(repository, "");
+
+			long latestRevision = repository.getLatestRevision();
+			System.out.println("Repository latest revision: " + latestRevision);
+
+		} catch (Exception e) {
+			throw e;
+		}
+
+	}
+
+	public static void listEntries(SVNRepository repository, String path) throws SVNException {
+		Collection entries = repository.getDir(path, -1, null, (Collection) null);
+		Iterator iterator = entries.iterator();
+		while (iterator.hasNext()) {
+			SVNDirEntry entry = (SVNDirEntry) iterator.next();
+			System.out.println("/" + (path.equals("") ? "" : path + "/") + entry.getName() + " ( author: '"
+					+ entry.getAuthor() + "'; revision: " + entry.getRevision() + "; date: " + entry.getDate() + ")");
+			if (entry.getKind() == SVNNodeKind.DIR) {
+				listEntries(repository, (path.equals("")) ? entry.getName() : path + "/" + entry.getName());
+			}
+		}
 	}
 
 }
